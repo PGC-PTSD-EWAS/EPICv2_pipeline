e4d2d75ce4e7a5f67c294c2079470c1fa4d68035
Seyma Katrinli <seyma.katrinli@emory.edu>
2024-07-24 12:14:45 -0400

    Update example_command.sh

--- example_command.sh
+++ example_command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Rscript --vanilla enmix_pipeline.R ~/idats.dir/ ~/testoutput.dir/ ~/other.dir/ NEW_2PLATES_ENMIX
+Rscript --vanilla enmix_pipeline.R ~/idats.dir/ ~/testoutput.dir/ ~/other.dir/ NEW_2PLATES_ENMIX ~/SampleSheet.csv
 
